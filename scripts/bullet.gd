b661c6c46d33877c7a53696b5365bbc2f130ced0
Nicolas Alves <nicolasalves136@gmail.com>
2024-07-10 16:04:28 -0300

    Add: Early combat system shot

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,5 +1,14 @@
 extends CharacterBody2D
-var speed : int = 30
+class_name Bullet
+
+var speed : int = 350
+
+func _ready() -> void:
+	_die()
 
 func _process(delta) -> void:
 	var colissionInfo = move_and_collide(velocity.normalized() * delta * speed)
+
+func _die() -> void:
+	await get_tree().create_timer(1).timeout
+	queue_free()
