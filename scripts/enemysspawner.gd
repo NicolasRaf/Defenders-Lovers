60e0d63a8956efdd94b51497e8b1c5787768e98b
ArthurV10 <arthurvms10@gmail.com>
2024-07-10 09:13:51 -0300

    feat: adding spawner enemys

---
+++ scripts/enemysspawner.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+#Carregando inimigo no cenario
+var enemy = preload("res://scenes/enemy.tscn")
+
+func spawner():
+	var enemy_instance = enemy.instantiate();
+	enemy_instance.position = Vector2(randi_range(0,10),randi_range(0,10))
+	add_child(enemy_instance)
+
+func _ready():
+	$Timer.start()
+
+func _on_timer_timeout():
+	spawner()
