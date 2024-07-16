7c02c3760cd938aec3f338a8df087c205de35f70
Nicolas Alves <nicolasalves136@gmail.com>
2024-07-16 09:29:19 -0300

    Add: Heart particules after shot enemy

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 @onready var animated_sprite_2d = $"../AnimatedSprite2D"
-
 func _ready():
 	Global.death = self;
 
@@ -22,6 +21,9 @@ func _on_body_entered(body):
 		collision_mask = 9
 		get_parent().collision_layer = 9
 		body.queue_free()
+		var heartParticules = preload("res://scenes/heartParticules.tscn").instantiate()
+		heartParticules.modulate = Color(1,1,1,1)
+		add_child(heartParticules)
 		await animated_sprite_2d.animation_finished
 		get_parent().queue_free()
 		Global.enemiesKilled += 1
